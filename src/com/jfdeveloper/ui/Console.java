a439a0a056f7818b659f58cbbf07076a268c4ffe
jfdoyle3 <36973560+jfdoyle3@users.noreply.github.com>
2021-07-26 20:47:29 -0400

    moved gui die display to it's own class

--- src/com/jfdeveloper/ui/Console.java
+++ src/com/jfdeveloper/ui/Console.java
@@ -2,7 +2,7 @@ package com.jfdeveloper.ui;
 
 public class Console {
 
-    public static void dieDisplay(int value) {
+    public static void dieDisplayLg(int value) {
         String[][] dice = new String[][]{
                 {"* * * * *", // Die 1 -> this line is dice[0][0]
                         "*       *",
@@ -18,6 +18,20 @@ public class Console {
                 }
         };
     }
+
+    public static void disDisplaySm(){
+        String[][] dice=new String[][]{
+                {"-----",
+                 "| 1 |",
+                 "-----"
+                },
+                {"-----",
+                 "| 2 |",
+                 "-----"
+                }
+        };
+    }
+
 
         public static void wizardBar(int health, int power, int mana){
             System.out.printf("Health: %d\tPower level: %d\tMana: %d",health,power,mana);
