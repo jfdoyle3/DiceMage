af2be1c06ad8343693d7e400e0df159afac8ab47
jfdoyle3 <36973560+jfdoyle3@users.noreply.github.com>
2021-08-12 05:57:08 -0400

    working on iterating thru dens

--- src/com/jfdeveloper/game/Game.java
+++ src/com/jfdeveloper/game/Game.java
@@ -1,5 +1,6 @@
 package com.jfdeveloper.game;
 
+import com.jfdeveloper.actors.Monster;
 import com.jfdeveloper.actors.Wizard;
 import com.jfdeveloper.ui.*;
 
@@ -7,25 +8,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class Game{
+public class Game {
 
     private Wizard wiz;
-//    private int wizards = 2;
+    //    private int wizards = 2;
     private WizardConsole console = new BWConsole();
     private int choice;
     private boolean turn;
-    private List<Wizard> wizards=new ArrayList<>();
+    private List<Wizard> wizards = new ArrayList<>();
 
 
     public Game() {
 
 
-    //   initWizards();
+        //   initWizards();
     }
 
     public void play() {
 
-        Turn turn=new Turn();
+        Turn turn = new Turn();
         turn.turn(wizards);
         System.out.println("game over");
 
@@ -40,51 +41,86 @@ public class Game{
         }
     }
 
-    public void testBuy(){
+    public void testBuy() {
 
     }
-    public void testWiz(){
-        Wizard g=new Wizard("G");
-        Wizard m=new Wizard("M");
+
+    public void testWiz() {
+        Wizard attacker = new Wizard("Attacker");
+        Wizard defender = new Wizard("Defender");
+
 
         // TODO: spawn monsters into den
-    //    g.getDen().spawnMonster(20);
-        m.getDen().spawnMonster(20);
-    //    g.getDen().spawnMonster(20);
-        m.getDen().spawnMonster(20);
+        attacker.getDen().spawnMonster(20);
+        attacker.getDen().spawnMonster(20);
+
+        defender.getDen().spawnMonster(20);
+        defender.getDen().spawnMonster(20);
 
         // TODO: Display Dens
-        System.out.println("G");
-        g.getDen().displayDen();
-        System.out.println();
-        System.out.println("M");
-        m.getDen().displayDen();
-        System.out.println();
-        System.out.println("\ng: "+g.getDen().isEmpty()+"\nm: "+m.getDen().isEmpty());
+        display(attacker, defender);
+//        System.out.println("\n\nis Den Empty");
+//        System.out.println("Attacker: " + attacker.getDen().isEmpty() + "\nDefender: " + defender.getDen().isEmpty());
+
+        // TODO: Get size of both dens
+        int denSizeAttacker = attacker.getDen().denSize();
+        int denSizeDefender = defender.getDen().denSize();
+
+        // TODO: Get Den List
+        List<Monster> monsterAttack = attacker.getDen().denAction();
+        List<Monster> monsterDefend = defender.getDen().denAction();
+
         // TODO: Attack rolls
-        int maxSize=0;
-//        if(g.getDen().isEmpty())
-        
-        m.getDen().denAction().get(0).getMonster().roll();
-        g.getDen().denAction().get(0).getMonster().roll();
+        int idx = 0;
+        while (!monsterAttack.isEmpty()) {
+            System.out.println("Round: "+idx);
+
+            display(attacker,defender);
+            if (defender.getDen().isEmpty()) {
+                System.out.println("no more");
+                break;
+//            for(int monster=0; monster<denSizeAttacker; monster++) {
+//                attacker.getDen().denAction().get(monster).getMonster().roll();
+                //  defender.dmgHealth();
+                //   }
+            } else {
+
+                System.out.println("defender has monsters.");
+            }
+        }
+        //    attacker.getDen().denAction().get(0).getMonster().roll();
+        //  defender.getDen().denAction().get(0).getMonster().roll();
 
         // TODO: get attack results
-        int mAttack=m.getDen().denAction().get(0).getMonster().getFaceUp();
-        int gAttack=g.getDen().denAction().get(0).getMonster().getFaceUp();
+
+        //  int attack = attacker.getDen().denAction().get(0).getMonster().getFaceUp();
+        //    int defend=defender.getDen().denAction().get(0).getMonster().getFaceUp();
 
         //TODO: display dens - show new rolls
-        System.out.println("\nG");
-        g.getDen().displayDen();
-        System.out.println("\nM");
-        m.getDen().displayDen();
+        display(attacker, defender);
 
         //TODO: determine who won
-        if(mAttack>gAttack)
-            System.out.println("\nM Wins");
+        System.out.println("\n-------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------------");
+//        if(defend>attack)
+//            System.out.println("\nDefender Wins");
+//
+//        else
+//            System.out.println("\nAttacker Wins");
 
-        else
-            System.out.println("\nG Wins");
+    }
 
+    private void display(Wizard attacker, Wizard defender) {
+        System.out.println("-----------------------------------------------");
+        System.out.println("Attacker");
+        System.out.println(attacker);
+        attacker.getDen().displayDen();
+        System.out.println("\n-----------------------------------------------");
+        System.out.println("Defender");
+        System.out.println(defender);
+        defender.getDen().displayDen();
+        System.out.println("\n-----------------------------------------------");
+        System.out.println("-----------------------------------------------");
     }
 
 }
